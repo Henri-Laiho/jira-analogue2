ef3773ff81121973649d8544f55cd079333bd11d
KristoferKSR <kaosaarkristofer@gmail.com>
2019-04-01 02:53:33 +0300

    added simple Lanterna TUI (check apacheUI package)

--- jira-analogue-team2/src/main/java/apacheUI/TUI.java
+++ jira-analogue-team2/src/main/java/apacheUI/TUI.java
@@ -1,23 +1,91 @@
 package apacheUI;
 
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
 
+import javax.swing.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class TUI{
+import static com.googlecode.lanterna.input.KeyType.ArrowDown;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
-        public static void main(String[] args) throws IOException {
-            Terminal terminal = new DefaultVirtualTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            TextGraphics tg = screen.newTextGraphics();
+public class TUI {
 
-            screen.startScreen();
-            tg.putString(9,9, "testing");
-            screen.refresh();
+    public static void main(String[] args) throws IOException {
+        List<String> projects = new ArrayList<>();
+        projects.add("project jira");
+        projects.add("onTime google");
+        projects.add("elephant rush");
+        projects.add("panda express");
+        projects.add("moodle hack");
+        projects.add("dong bong");
+        projects.add("helper");
+        projects.add("python for java programmers");
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+
+        Screen screen = new TerminalScreen(terminal);
+        TextGraphics tg = screen.newTextGraphics();
+
+        screen.startScreen();
+        tg.putString(0, 1, "welcome to the minjira text user interface");
+        tg.putString(0, 2, "select project name (up and down arrow, hit enter to select):");
+        boolean running = true;
+        int i = 0;
+
+        while (running) {
+
+            KeyStroke pressed = terminal.pollInput();
+
+            if (pressed != null) {
+                System.out.println(pressed);
+                switch ((pressed.getKeyType())) {
+                    case Escape:
+                        running = false;
+                        break;
+
+                    case ArrowUp:
+                        if (i < projects.size() - 1) {
+                            //small glitch:
+                            //reaching the last element makes it go back to the second to last
+                            //also instantly assumes that enter is hit
+                            i += 1;
+                            tg.putString(0, 3, "                            ");
+                            tg.putString(0, 3, projects.get(i), SGR.BOLD);
+                            break;
+                        }
+
+                    case ArrowDown:
+                        if (i > 0) {
+                            i += -1;
+                            tg.putString(0, 3, "                            ");
+                            tg.putString(0, 3, projects.get(i), SGR.BOLD);
+                            break;
+                        }
+                    case Enter:
+                        tg.putString(0, 4, "                                                                                      ");
+                        tg.putString(0,4, "you have selected: " + projects.get(i));
+                        //insert opening project method/new screen method
+                        //
+                        //here
+                        break;
+                }
+                screen.refresh();
+            }
 
         }
+        screen.refresh();
+        screen.readInput();
+        screen.stopScreen();
+        System.exit(0);
+
+    }
 }
